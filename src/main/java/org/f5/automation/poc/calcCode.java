c66ca635338d07b81c3285b5793d0e8b7598fa03
S.GAMPA <S.GAMPA@HYD-L-00062414.olympus.F5Net.com>
2019-05-22 15:56:11 +0530

    test

---
+++ src/main/java/org/f5/automation/poc/calcCode.java
@@ -0,0 +1,12 @@
+package org.f5.automation.poc;
+
+public class calcCode {
+	
+	public int absoluteVal(int a)
+	{
+		if(a < 0)
+			return -1*a;
+		else
+			return a;
+	}
+}
---
+++ src/test/java/org/f5/automation/poc/calc.java
@@ -0,0 +1,22 @@
+package org.f5.automation.poc;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class calc {
+	
+	@Test
+	public void TestAbsVal1()
+	{
+		calcCode obj = new calcCode();
+		Assert.assertEquals(obj.absoluteVal(2), 2);
+	}
+	
+	//@Test
+	public void TestAbsVal2()
+	{
+		calcCode obj = new calcCode();
+		Assert.assertEquals(obj.absoluteVal(-2), 2);
+	}
+	
+}
